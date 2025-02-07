e36eff6dd1c00894f104d18a56fb88e2d4832c30
pedrokekww <pedroveb119@gmail.com>
2025-02-07 14:06:55 -0300

    Create Instancia.java

---
+++ Instancia.java
@@ -0,0 +1,7 @@
+    public static void main(String[] args) {
+        Usuario user1 = new Usuario();
+        Usuario user2 = new Usuario();
+        Usuario user3 = new Usuario();
+        System.out.println("Número total de usuários criados: " + Usuario.getContador());
+    }
+}
